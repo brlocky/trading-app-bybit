d863285681d6ee4af8a66299f36d667e198c89cb
Joao <brlocky@gmail.com>
2023-04-25 01:46:23 +0100

    Layout

--- src/layouts/Guest.tsx
+++ src/layouts/Guest.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import NavBar from '../components/Navbars/NavBar';
+import tw from 'twin.macro';
+
+const BodyComponent = tw.div`
+mx-auto
+max-w-screen-2xl
+pt-5
+pb-5
+pl-10
+pr-10
+flex
+flex-col
+gap-1
+`;
 
 const GuestLayout: React.FC = () => {
   return (
     <>
       <NavBar />
 
-      <div className="p-4">
+      <BodyComponent>
         <Outlet />
-      </div>
+      </BodyComponent>
     </>
   );
 };
