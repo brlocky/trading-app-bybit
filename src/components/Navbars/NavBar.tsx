a98fd9e31163e31a0fe9abf5a3a79ced4d3ef821
Joao <brlocky@gmail.com>
2023-05-31 23:52:52 +0100

    Show Version on UI

--- src/components/Navbars/NavBar.tsx
+++ src/components/Navbars/NavBar.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import packageJson from '../../../package.json';
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = React.useState(false);
+
+  const version = packageJson.version;
   return (
     <>
       <div className="flex flex-wrap py-2">
         <div className="w-full">
           <nav className="relative flex flex-wrap items-center justify-between rounded bg-blue-400 py-3">
-            <div className="w-full mx-auto flex flex-wrap items-center justify-between px-4">
+            <div className="mx-auto flex w-full flex-wrap items-center justify-between px-4">
               <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
-                <Link
-                  to="/"
-                  className="flex items-center px-3 py-2 text-xs font-bold uppercase leading-snug text-white hover:opacity-75"
-                >
-                  <i className="fas fa-globe leading-lg"></i> Trading
+                <Link to="/" className="flex items-center gap-x-1 py-2 leading-snug text-white hover:opacity-75">
+                  <i className="fas fa-globe leading-lg"></i> <span className="text-xs font-bold uppercase">Trading</span><span className="text-xs ">v{version}</span>
                 </Link>
                 <button
                   className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none text-white outline-none focus:outline-none lg:hidden"
@@ -24,10 +24,7 @@ export default function NavBar() {
                   <i className="fas fa-bars"></i>
                 </button>
               </div>
-              <div
-                className={'flex-grow items-center lg:flex' + (menuOpen ? ' flex' : ' hidden')}
-                id="example-navbar-info"
-              >
+              <div className={'flex-grow items-center lg:flex' + (menuOpen ? ' flex' : ' hidden')} id="example-navbar-info">
                 <ul className="flex list-none flex-col lg:ml-auto lg:flex-row">
                   <li className="nav-item">
                     <Link
