a96d043e452f32f37e37a940304cf45a31e8adc2
Joao Cunha <brlocky@gmail.com>
2024-02-20 15:50:14 +0000

    Missing file

--- src/components/Cards/CardSymbol.tsx
+++ src/components/Cards/CardSymbol.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { LeverageSelector, PositionSizeSelector } from '../Trade';
 import { useSelector } from 'react-redux';
-import { selectCurrentPosition, selectSymbol } from '../../store/slices';
+import { selectSymbol } from '../../store/slices';
 import { CardLastTrades } from './CardLastTrades';
 import { Tabs } from '../Tabs';
 import CardOrderSettings from './CardOrderSettings';
 
 export const CardSymbol: React.FC = () => {
-  const currentPosition = useSelector(selectCurrentPosition);
   const currentSymbol = useSelector(selectSymbol);
   return (
-    <div className="justify-top flex flex-col gap-y-3">
+    <div className="justify-top flex h-full flex-col gap-y-3">
       {/* {!currentPosition ? <PositionSizeSelector /> : null} */}
       {currentSymbol ? (
         <Tabs
@@ -32,9 +31,6 @@ export const CardSymbol: React.FC = () => {
       ) : null}
       <LeverageSelector />
       <CardLastTrades />
-      {/* <pre>{JSON.stringify(wallet, null, 2)}</pre> */}
-      {/* <pre>{JSON.stringify(symbolProps, null, 2)}</pre> */}
-      {/* <pre>{JSON.stringify(symbolInfo, null, 2)}</pre> */}
     </div>
   );
 };
